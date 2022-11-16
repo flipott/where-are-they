839b2dc23cc6f51431e889d8793712abbe187653
flipott <ottphillipa@gmail.com>
2022-11-16 16:56:18 -0500

    Build page skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React from "react";
+import "./css/App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Timer from "./components/Timer";
+import ArtistsToFind from "./components/ArtistsToFind";
 
 function App() {
   return (
     <div className="App">
-      Where Are They?
+      <Header />
+      <div className="main">
+        <div className="description-leaderboard">
+          <button className="empty-btn">Leaderboard</button>
+          <p className="description">Click on their heads to identify them!</p>
+          <button>Leaderboard</button>
+        </div>
+        <div className="game">
+          <div className="game-left">
+            <Timer />
+            <button>Start Game</button>
+            <ArtistsToFind />
+          </div>
+          <div className="main-img"><img src={require('./images/main.jpg')} /></div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
