5f5406d7729ac141983a1df4a3373edadb2fb03a
flipott <ottphillipa@gmail.com>
2022-11-23 18:34:34 -0500

    Implement functions to fetch artist thumbnails

--- src/components/ArtistsToFind.js
+++ src/components/ArtistsToFind.js
@@ -3,24 +3,28 @@ import "../css/ArtistsToFind.css";
 import { storage } from "../firebase";
 import { getStorage, ref, getDownloadURL, listAll } from "/node_modules/firebase/storage";
 
-
-const testArtist = ["Bjork"]
-
 export default function ArtistsToFind(props) {
     
     const [files, setFiles] = React.useState('');
     const { toFind } = props;
     useEffect(() => {
-        console.log(files);
-        let urlTest;
         const getArtistImages = async () => {
             const artistImages = ref(storage, 'artist-photos/')
             listAll(artistImages)
                 .then(async (res) => {
                 const { items } = res;
                 const urls = await Promise.all(items.map((item) => getDownloadURL(item)));
-                urlTest = urls;
-                setFiles(urls);
+                const names = await Promise.all(items.map((item) => item._location.path_.slice(14, -4)));
+                let completeArr = [];
+
+                for (let i = 0; i < names.length; i++) {
+                    const newObj = {
+                        artistName: names[i],
+                        imgUrl: urls[i]
+                    }
+                    completeArr.push(newObj);
+                }
+                setFiles(completeArr);
             })
             .catch((error) => {
                 console.log(error);
@@ -30,48 +34,29 @@ export default function ArtistsToFind(props) {
         getArtistImages();
     }, [])
 
-    const items = toFind.map((artist) => 
-        <div className="artist-item">
-            <img src={require('../images/bjork.png')} />
-            <p>{artist}</p>
-            <div className="find-status">Not Found</div>
-        </div>
-    )
 
-    // const items = toFind.map((name) => <h1>{name}</h1>)
-
-
+    function getImageIndex(artist) {
+        const artistName = artist.split(" ").join("").toLowerCase();
+        for (let i = 0; i < files.length; i++) {
+            if (files[i].artistName === artistName) {
+                return files[i].imgUrl;
+            }
+        }
+    }
 
     return (
         <div className="artists-to-find">
             <h2>Artists to Find</h2>
             <div className="item-section">
-                {items}
+                {files && 
+                toFind.map((artist) => 
+                    <div className="artist-item" key={artist}>
+                        <img src={getImageIndex(artist)} />
+                        <p>{artist}</p>
+                        <div className="find-status">Not Found</div>
+                    </div>
+                )}
             </div>
         </div>
     )
 }
-
-// async function checkSelection(artistName, point) {
-//     console.log(`Artist: ${artistName} Coords: ${point}`)
-//     let currentArray = null;
-
-//     const docRef = doc(db, "artists", artistName);
-//     const docSnap = await getDoc(docRef);
-
-//     if (docSnap.exists()) {
-//       currentArray = docSnap.data().coords;
-//       console.log(docSnap.data().image);
-//     } else {
-//       console.error("Document does not exist.");
-//     }
-
-//     const parseArr = JSON.parse(currentArray);
-
-//     if (inside(point, parseArr)) {
-//       console.log("YES!");
-//     } else {
-//       console.log("NO!");
-//     }
-
-//   }
