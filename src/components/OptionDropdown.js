7b67d78b68e995bda4f223039668440d75d624b5
flipott <ottphillipa@gmail.com>
2022-11-23 19:31:37 -0500

    Fix prop behavior

--- src/components/OptionDropdown.js
+++ src/components/OptionDropdown.js
@@ -6,7 +6,7 @@ export default function OptionDropdown(props) {
 
 
     const nameItems = names.map(name =>
-         <button value={name} key={name} onClick={() => optionSelect(name)} className="dropdown-btn">{name}</button>)
+         <button value={name.artistName} key={name.artistName} onClick={() => optionSelect(name.artistName)} className="dropdown-btn">{name.artistName}</button>)
     return (
         <div className="option-dropdown" style={{left: (positionEvent.pageX - 23 + "px"), top: (positionEvent.pageY + 18 + "px") }}>
             {nameItems}
