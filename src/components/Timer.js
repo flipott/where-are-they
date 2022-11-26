c837b6a2a52b33eb1183ca5470ff03b0ca25b7e6
flipott <ottphillipa@gmail.com>
2022-11-25 19:20:04 -0500

    Add logic for time

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../css/Timer.css";
 
-export default function Timer() {
-    return <div className="timer">00:00</div>
+export default function Timer(props) {
+
+    const [currentTime, setCurrentTime] = React.useState(0);
+
+    useEffect(() => {
+        props.setFinalTime(finalTime);
+        let interval;
+        interval = setInterval(() => {
+            setCurrentTime((prevTime) => prevTime + 10);
+        }, 10);
+
+        return () => clearInterval(interval);
+    }, [props.toFind])
+
+    const mins = ("0" + Math.floor((currentTime / 60000) % 60)).slice(-2) + ":";
+    const seconds = ("0" + Math.floor((currentTime / 1000) % 60)).slice(-2) + ":";
+    const milliseconds = ("0" + ((currentTime / 10) % 100)).slice(-2);
+    const finalTime = mins+seconds+milliseconds;
+
+    return (
+        <div className="timer">
+            <div className="nums">
+                <p>{mins}</p>
+                <p>{seconds}</p>
+                <p>{milliseconds}</p>
+            </div>
+        </div>
+    );
 }
 
