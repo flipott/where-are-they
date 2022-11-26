7f30dc80f0ed834e7648fc7608ccf5844e313689
flipott <ottphillipa@gmail.com>
2022-11-25 19:20:30 -0500

    Refactor so that fetching artist data and images is quicker

--- src/components/ArtistsToFind.js
+++ src/components/ArtistsToFind.js
@@ -1,41 +1,14 @@
 import React, { useEffect } from "react";
 import "../css/ArtistsToFind.css";
-import { storage } from "../firebase";
-import { getStorage, ref, getDownloadURL, listAll } from "/node_modules/firebase/storage";
 
 export default function ArtistsToFind(props) {
     
-    const [files, setFiles] = React.useState('');
-    const { toFind } = props;
-    useEffect(() => {
-        const getArtistImages = async () => {
-            const artistImages = ref(storage, 'artist-photos/')
-            listAll(artistImages)
-                .then(async (res) => {
-                const { items } = res;
-                const urls = await Promise.all(items.map((item) => getDownloadURL(item)));
-                const names = await Promise.all(items.map((item) => item._location.path_.slice(14, -4)));
-                let completeArr = [];
+    const { toFind, files } = props;
 
-                for (let i = 0; i < names.length; i++) {
-                    const newObj = {
-                        artistName: names[i],
-                        imgUrl: urls[i]
-                    }
-                    completeArr.push(newObj);
-                }
-                setFiles(completeArr);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        }
-
-        getArtistImages();
-    }, [])
 
 
     function getImageIndex(artist) {
+        console.log("start")
         const artistName = artist.split(" ").join("").toLowerCase();
         for (let i = 0; i < files.length; i++) {
             if (files[i].artistName === artistName) {
@@ -50,7 +23,7 @@ export default function ArtistsToFind(props) {
             <div className="item-section">
                 {files && 
                 toFind.map((artist) => 
-                    <div className="artist-item" key={artist}>
+                    <div className="artist-item" key={artist} >
                         <img src={getImageIndex(artist.artistName)} />
                         <p>{artist.artistName}</p>
                         <div className={artist.found ? "found" : "not-found"}>{artist.found ? "Found" : "Not Found"}</div>
