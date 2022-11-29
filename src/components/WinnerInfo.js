ae3499702395706ce0ddfa2da744d4666cd97599
flipott <ottphillipa@gmail.com>
2022-11-28 20:16:23 -0500

    Implement form submission logic and ability to replay game

--- src/components/WinnerInfo.js
+++ src/components/WinnerInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/WinnerInfo.css"
 
 export default function WinnerInfo(props) {
     const { finalTime, setWinner } = props;
@@ -18,12 +19,14 @@ export default function WinnerInfo(props) {
     return (
         <div className="winner">
             <p className="final-time">Success! Your final time was: <strong>{finalTime}</strong></p>
-            <p className="name-entry">Enter your name below to be added to the leaderboard.</p>
-            <div className="name-form">
-                <input type="text" maxLength={12} value={textVal} onChange={(e) => setTextVal(e.target.value)} disabled={submitted} />
-                <button onClick={handleSubmit} disabled={submitted}>Submit</button>
+            <div className="name-option">
+                <p className="name-entry">Enter your name below to be added to the leaderboard.</p>
+                <div className="name-form">
+                    <input type="text" maxLength={12} value={textVal} onChange={(e) => setTextVal(e.target.value)} disabled={submitted} />
+                    <button onClick={handleSubmit} disabled={submitted}>Submit</button>
+                </div>
+                {submitted && <p className="submitted">Your score has been successfully added to the leaderboard.</p>}
             </div>
-            {submitted && <p className="submitted">Your score has been successfully added to the leaderboard.</p>}
             <button onClick={() => setWinner(false)}>Play Again</button>
         </div>
     )
