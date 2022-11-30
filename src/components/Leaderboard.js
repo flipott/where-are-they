1ffab9f86b76e5eedf648c81d3f12ae8ecf585c3
flipott <ottphillipa@gmail.com>
2022-11-29 20:07:48 -0500

    Render leaderboard information dynamically

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -3,68 +3,30 @@ import "../css/Leaderboard.css";
 
 export default function Leaderboard(props) {
 
-
+    const rows = props.leaderScores.map((obj, ind) => {
+        return (
+            <tr>
+                <td>{ind+1}</td>
+                <td>{obj.player}</td>
+                <td>{obj.time}</td>
+            </tr>
+            )
+    })
+    
     return (
         <div className="leaderboard-modal">
             <div className="leaderboard">
                 <button onClick={() => props.setDisplayLeaderboard(false)}>x</button>
                 <h2>Leaderboard</h2>
                 <table>
-                    <tr>
-                        <th>Place</th>
-                        <th>Player</th>
-                        <th>Time</th>
-                    </tr>
-                    <tr>
-                        <td>1.</td>
-                        <td>Phil</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>2.</td>
-                        <td>Jerry</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>3.</td>
-                        <td>Antonio</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>4.</td>
-                        <td>Barrelous</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>5.</td>
-                        <td>Anrew</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>6.</td>
-                        <td>Karel</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>7.</td>
-                        <td>Amoun</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>8.</td>
-                        <td>Barnem</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>9.</td>
-                        <td>Gregory</td>
-                        <td>00:02:03</td>
-                    </tr>
-                    <tr>
-                        <td>10.</td>
-                        <td>Kristi</td>
-                        <td>00:02:03</td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <th>Place</th>
+                            <th>Player</th>
+                            <th>Time</th>
+                        </tr>
+                        {rows}
+                    </tbody>
                 </table>
             </div>
         </div>
