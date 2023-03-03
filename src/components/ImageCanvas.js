de62af26724fbdd965bb450f2a17c7ca9d534213
flipott <ottphillipa@gmail.com>
2023-03-03 02:02:09 -0500

    Add different ratio calculations based on page size

--- src/components/ImageCanvas.js
+++ src/components/ImageCanvas.js
@@ -7,25 +7,65 @@ export default function ImageCanvas(props) {
     const [active, setActive] = React.useState(false);
     const [positionEvent, setPositionEvent] = React.useState(null);
     const [canvas, setCanvas] = React.useState(null);
+    const [innerWidth, setInnerWidth] = React.useState(window.innerWidth);
+    const [ratio, setRatio] = React.useState("large");
+
+    useEffect(() => {
+      if (innerWidth >= 1000) {
+        setRatio("large");
+      } else if (innerWidth < 1000 && innerWidth >= 600 ) {
+        setRatio("medium");
+      } else {
+        setRatio("small")
+      }
+    }, [innerWidth]);
+    
+    useEffect(() => {
+      function handleResize() {
+        setInnerWidth(window.innerWidth);
+      }
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
 
     useEffect(() => {
         const imgCanvas = document.getElementById('img-canvas');
         const context = imgCanvas.getContext("2d");
         const img = new Image();
+
+        if (ratio === "small") {
+          imgCanvas.width = 294 * devicePixelRatio;
+          imgCanvas.height = 350 * devicePixelRatio;
+        } else if (ratio === "medium") {
+          imgCanvas.width = 412 * devicePixelRatio;
+          imgCanvas.height = 490 * devicePixelRatio;
+        } else {
+          imgCanvas.width = 588 * devicePixelRatio;
+          imgCanvas.height = 700 * devicePixelRatio;  
+        }
+
         img.src = require('../images/main.jpg');
-        imgCanvas.width = 588 * devicePixelRatio;
-        imgCanvas.height = 700 * devicePixelRatio;
         imgCanvas.style.width = (imgCanvas.width / devicePixelRatio).toString() + "px";
         imgCanvas.style.height = (imgCanvas.height / devicePixelRatio).toString() + "px";
 
         const blankCanvas = document.getElementById('blank-canvas');
-        blankCanvas.width = 588 * devicePixelRatio;
-        blankCanvas.height = 700 * devicePixelRatio;
+        if (ratio === "small") {
+          blankCanvas.width = 294 * devicePixelRatio;
+          blankCanvas.height = 350 * devicePixelRatio;  
+        } else if (ratio === "medium") {
+          blankCanvas.width = 412 * devicePixelRatio;
+          blankCanvas.height = 490 * devicePixelRatio;
+        } else {
+          blankCanvas.width = 588 * devicePixelRatio;
+          blankCanvas.height = 700 * devicePixelRatio;  
+        }
         blankCanvas.style.width = (blankCanvas.width / devicePixelRatio).toString() + "px";
         blankCanvas.style.height = (blankCanvas.height / devicePixelRatio).toString() + "px";
 
         img.onload = () => context.drawImage(img, 0, 0, imgCanvas.width, imgCanvas.height);
-      }, []);
+      },[ratio]);
 
     function handleClick(e) {
         setPositionEvent(e);
@@ -39,8 +79,16 @@ export default function ImageCanvas(props) {
       drawCircle(positionEvent, canvas);
       setActive(false);
       const coords = getMousePosition(canvas, positionEvent);
-      const coordArr = [parseInt(coords.x), parseInt(coords.y)];
-      props.checkSelection(name, coordArr);
+      let coordArr = [parseInt(coords.x), parseInt(coords.y)];
+      // console.log(coordArr);
+      console.log([parseInt(coordArr[0] * 2), parseInt(coordArr[1] * 2)]);
+      if (ratio === "small") {
+        props.checkSelection(name, [parseInt(coordArr[0] * 2), parseInt(coordArr[1] * 2)])
+      } else if (ratio === "medium") {
+        props.checkSelection(name, [parseInt(coordArr[0] * 1.7), parseInt(coordArr[1] * 1.7)] );
+      } else {
+        props.checkSelection(name, coordArr);
+      }
     }
 
     function drawCircle(position, canvas) {
