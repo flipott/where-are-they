7845e733e95cccfe682394c28ed698b2f1f54169
flipott <ottphillipa@gmail.com>
2023-03-03 23:58:20 -0500

    Update year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,5 @@ import React from "react";
 import "../css/Footer.css";
 
 export default function Footer() {
-    return <p className="footer">Copyright © 2022 Phillip Ott. Drawing by @calxlea.</p>
+    return <p className="footer">Copyright © 2023 Phillip Ott. Drawing by @calxlea.</p>
 }
